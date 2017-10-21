1807cb38ea5a2e7f4d270deb114059532e1c4e4b
Nick K <nick.kruchinin@gmail.com>
2017-10-22 02:56:23 +0300

    Remove unused tests.

--- src/test/FormData/common.js
+++ src/test/FormData/common.js
@@ -3,13 +3,17 @@ import {Readable} from "stream"
 import test from "ava"
 
 import pq from "proxyquire"
+import req from "supertest"
 
 import {spy} from "sinon"
+import {createReadStream, readFile} from "promise-fs"
 
 import boundary from "../../lib/util/boundary"
 import FormData from "../../lib/FormData"
 
 import count from "../__helper__/count"
+import read from "../__helper__/read"
+import server from "../__helper__/server"
 
 test("The stream accessor should return Readable stream", t => {
   t.plan(1)
@@ -104,3 +108,68 @@ test("Should add initial fields from a collection", t => {
   t.is(fd.get("nick"), "Rarara")
   t.is(fd.get("eyes"), "blue")
 })
+
+test("Should correctly add a filed to FormData request body", async t => {
+  t.plan(1)
+
+  const fd = new FormData()
+
+  const field = "Hello, World!"
+
+  fd.set("field", field)
+
+  const data = await read(fd)
+
+  const {body} = await req(server())
+    .post("/")
+    .set("content-type", `multipart/form-data; boundary=${fd.boundary}`)
+    .send(data)
+
+  t.deepEqual(body, {field})
+})
+
+test("Should correctly add a file to FormData request body", async t => {
+  t.plan(1)
+
+  const fd = new FormData()
+
+  fd.set("file", createReadStream("/usr/share/dict/words"))
+
+  const file = await readFile("/usr/share/dict/words", "utf8")
+
+  const data = await read(fd)
+
+  const {body} = await req(server())
+    .post("/")
+    .set("content-type", `multipart/form-data; boundary=${fd.boundary}`)
+    .send(data)
+
+  t.deepEqual(body, {file})
+})
+
+test(
+  "Should correctly add field AND file together to FormData request body",
+  async t => {
+    t.plan(1)
+
+    const fd = new FormData()
+
+    const field = "Hello, World!"
+
+    fd.set("field", field)
+
+    fd.set("file", createReadStream(__filename))
+
+    const file = await readFile(__filename, "utf8")
+
+    const data = await read(fd)
+
+    const {body} = await req(server())
+      .post("/")
+      .set("content-type", `multipart/form-data; boundary=${fd.boundary}`)
+      .send(data)
+
+    t.deepEqual(body, {field, file})
+  }
+)
+
