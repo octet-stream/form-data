dedc08ebdd810bac82132a504bd35b993f23ea94
Nick K <nick.kruchinin@gmail.com>
2018-09-18 01:41:43 +0300

    Rename .babelrc -> .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,14 +1,14 @@
-{
-  "plugins": [
+module.exports = {
+  plugins: [
     "@babel/transform-runtime",
     ["@babel/proposal-decorators", {
-      "legacy": true
+      legacy: true
     }],
     "@babel/proposal-class-properties",
     "@babel/transform-async-to-generator",
     "@babel/proposal-async-generator-functions",
     ["@babel/transform-modules-commonjs", {
-      "mjsStrictNamespace": false
+      mjsStrictNamespace: false
     }]
   ]
 }
