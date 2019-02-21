add2d95f6f073b335c343b5ae54fd6bb8d50040c
Nick K <nick.kruchinin@gmail.com>
2019-02-22 01:43:05 +0300

    Add more tests for FormData#getComputedLength() method.

--- src/test/FormData/getComputedLength.mjs
+++ src/test/FormData/getComputedLength.mjs
@@ -1,8 +1,10 @@
 import stream from "stream"
+import fs from "fs"
 
 import test from "ava"
 
 import FormData from "../../lib/FormData"
+import read from "../__helper__/readStreamWithAsyncIterator"
 
 test("Should return 0 when FormData have no fields", async t => {
   const fd = new FormData()
@@ -22,3 +24,45 @@ test("Should return 0 when FormData have stream.Readable fields", async t => {
 
   t.is(actual, 0)
 })
+
+test(
+  "Should correctly compute content length of the FormData with regular field",
+  async t => {
+    const fd = new FormData()
+
+    fd.set("name", "Nyx")
+
+    const actual = await fd.getComputedLength()
+    const expected = await read(fd).then(({length}) => length)
+
+    t.is(actual, expected)
+  }
+)
+
+test(
+  "Should correctly compute content length of the FormData with Buffer",
+  async t => {
+    const fd = new FormData()
+
+    fd.set("field", Buffer.from("Just another string"))
+
+    const actual = await fd.getComputedLength()
+    const expected = await read(fd).then(({length}) => length)
+
+    t.is(actual, expected)
+  }
+)
+
+test(
+  "Should correctly compute content length of the FormData with file",
+  async t => {
+    const fd = new FormData()
+
+    fd.set("file", fs.createReadStream("/usr/share/dict/words"))
+
+    const actual = await fd.getComputedLength()
+    const expected = await read(fd).then(({length}) => length)
+
+    t.is(actual, expected)
+  }
+)
