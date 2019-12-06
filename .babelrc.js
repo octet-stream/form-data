d45a2226b9f354e0cf04f61fbdf999cd6adbad90
Nick K <nick.kruchinin@gmail.com>
2019-12-06 03:31:23 +0300

    Remove unnecessary babel plugins and polyfills

--- .babelrc.js
+++ .babelrc.js
@@ -1,14 +1,10 @@
 const config = {
   plugins: [
-    ["@babel/transform-runtime", {
-      corejs: 3
-    }],
     ["@babel/proposal-decorators", {
       legacy: true
     }],
     "@babel/proposal-class-properties",
-    "@babel/transform-async-to-generator",
-    "@babel/proposal-async-generator-functions"
+    "@babel/transform-runtime",
   ]
 }
 
