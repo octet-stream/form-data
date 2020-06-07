5c825798c557e1f98e0e36c876705e383b85d80b
octet-stream <nick.kruchinin@gmail.com>
2020-06-07 19:08:34 +0300

    Remove unnecessary comment.

--- src/test/__helper__/server.mjs
+++ src/test/__helper__/server.mjs
@@ -6,7 +6,6 @@ import toObject from "object-deep-from-entries"
 import {parse} from "then-busboy"
 
 const server = () => http.createServer((req, res) => {
-  // TODO: Rewrite this function due to upcoming then-busboy changes
   async function transform(body) {
     const files = await Promise.all(body.files.entries().map(
       ([path, file]) => fs.promises.readFile(file.path).then(content => [
