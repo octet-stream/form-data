b6ab4e8b1a4d4de306b71b7f4cd05df2d179f558
Nick K <nick.kruchinin@gmail.com>
2021-04-05 14:54:45 +0300

    Add docs for fileFromPath

--- lib/fileFromPath.ts
+++ lib/fileFromPath.ts
@@ -5,6 +5,13 @@ import blobFromPathSync from "fetch-blob/from.js"
 
 import {File, FileOptions} from "./File"
 
+/**
+ * Creates a new File from Blob backed by filesystem
+ *
+ * @param path Path to read a file from
+ * @param filename Optional file name. If not presented, the path will be used to get it
+ * @param options File options
+ */
 export function fileFromPathSync(
   path: string,
   filename?: string,
