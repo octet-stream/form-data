b843b90681da987e68e8291768e38758e168ede5
Nick K <nick.kruchinin@gmail.com>
2021-07-25 22:52:40 +0300

    Code style fixes for browser.js to fit with the entire project. Add JSDoc for browser.js exports.

--- browser.js
+++ browser.js
@@ -1,10 +1,16 @@
-const accessValue = (name) =>
-  globalThis !== 'undefined' // new standardized access to the global object
+const accessValue = name => (
+  globalThis !== "undefined" // new standardized access to the global object
     ? globalThis[name]
-    : typeof self !== 'undefined' // WebWorker specific access
+    : typeof self !== "undefined" // WebWorker specific access
     ? self[name]
-    : window[name];
+    : window[name]
+)
 
-export const FormData = accessValue('FormData');
-export const Blob = accessValue('Blob');
-export const File = accessValue('File');
+/** @type {FormData} */
+export const FormData = accessValue('FormData')
+
+/** @type {Blob} */
+export const Blob = accessValue('Blob')
+
+/** @type {File} */
+export const File = accessValue('File')
