eeedec270a8c6157e0b07ec249cd8328af4189fe
Nick K <nick.kruchinin@gmail.com>
2021-08-11 17:35:17 +0300

    Add separate tsconfig for AVA.

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,13 +1,12 @@
 module.exports = {
   environmentVariables: {
-    TS_NODE_COMPILER: "ttypescript",
-    TS_NODE_PROJECT: "tsconfig.cjs.json"
+    TS_NODE_PROJECT: "tsconfig.ava.json"
   },
   extensions: [
     "ts"
   ],
   require: [
-    "ts-node/register/transpile-only",
+    "ts-node/register",
   ],
   files: [
     "lib/**/*.test.ts"
