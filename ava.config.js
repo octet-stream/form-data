d6a1298f7bdae10dca98963201ca05e38b0898e7
Nick K <nick.kruchinin@gmail.com>
2023-05-19 19:29:04 +0300

    Update AVA configuration with workaround for Node v20

--- ava.config.js
+++ ava.config.js
@@ -1,11 +1,11 @@
 export default {
+  failFast: true,
+  environmentVariables: {
+    TS_NODE_PROJECT: "./tsconfig.ava.json"
+  },
   extensions: {
     ts: "module"
   },
-  nodeArguments: [
-    "--no-warnings",
-    "--loader=ts-node/esm/transpile-only"
-  ],
   files: [
     "src/**/*.test.ts"
   ]
