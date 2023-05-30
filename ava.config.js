f7fda271398d95693b4e11965a93cc64816e2f7a
Nick K <nick.kruchinin@gmail.com>
2023-05-30 18:00:45 +0300

    Port AVA workaround for Node 20

--- ava.config.js
+++ ava.config.js
@@ -1,10 +1,13 @@
 export default {
+  failFast: true,
+  environmentVariables: {
+    TS_NODE_PROJECT: "tsconfig.ava.json"
+  },
   extensions: {
     ts: "module"
   },
   nodeArguments: [
-    "--no-warnings",
-    "--loader=ts-node/esm/transpile-only"
+    "--no-warnings"
   ],
   files: [
     "src/**/*.test.ts"
